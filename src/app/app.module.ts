6305509b3d2404d11b84487763f66389ac1acf1d
vivek1996 <mvivek3112@gmail.com>
2018-06-15 12:25:29 +0530

    about component finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   schemas: [NO_ERRORS_SCHEMA],
   providers: [],
