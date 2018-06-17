04905d7e3353bdcf52fbe6bc101cf6c648343173
vivek1996 <mvivek3112@gmail.com>
2018-06-17 22:42:33 +0530

    final commit

--- src/app/house-view/house-view.component.ts
+++ src/app/house-view/house-view.component.ts
@@ -5,19 +5,34 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-house-view',
   templateUrl: './house-view.component.html',
-  styleUrls: ['./house-view.component.scss']
+  styleUrls: ['./house-view.component.scss'],
 })
 export class HouseViewComponent implements OnInit {
   public houseId;
   public house;
-  constructor(private _http: GotService,
+  constructor(
+    private _http: GotService,
     private route: ActivatedRoute,
-    private location: Location) {
-      this.houseId = this.route.snapshot.paramMap.get('houseId');
-    }
+    private location: Location
+  ) {
+    this.houseId = this.route.snapshot.paramMap.get('houseId');
+  }
 
   ngOnInit() {
     this._http.getSingleHouse(this.houseId).subscribe(
+      data => {
+        this.house = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+    this.route.params.subscribe(routeParams => {
+      this.loadDetail(routeParams.houseId);
+    });
+  }
+  public loadDetail(id) {
+    this._http.getSingleHouse(id).subscribe(
       data => {
         this.house = data;
       },
@@ -29,5 +44,4 @@ export class HouseViewComponent implements OnInit {
   public goBackToPreviousPage() {
     this.location.back();
   }
-
 }
